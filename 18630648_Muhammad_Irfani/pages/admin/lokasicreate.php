c35938ae551649617a3108619e4bd571b934bc79
irfani12 <96363883+irfani12@users.noreply.github.com>
2022-01-31 11:29:30 +0800

    Membuat Halaman Tambah Data

---
+++ 18630648_Muhammad_Irfani/pages/admin/lokasicreate.php
@@ -0,0 +1,41 @@
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb2">
+            <div class="col-sm-6">
+                <h1>Tambah Data Lokasi</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="?page=home">Home</a></li>
+                    <li class="breadcrumb-item"><a href="?page=lokasiread">Lokasi</a></li>
+                    <li class="breadcrumb-item">Tambah Data</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Tambah Lokasi</h3>
+        </div>
+        <div class="card-body">
+            <form action="" method="post">
+                <div class="form-group col-6">
+                    <label for="nama_lokasi">Nama Lokasi</label>
+                    <input type="text" class="form-control" name="nama_lokasi" id="nama_lokasi">
+                </div>
+                <div class="form-group col-6">
+                    <a href="?page=lokasiread" class="btn btn-danger btn-sm float-right">
+                        <i class="fa fa-times"></i> Batal
+                    </a>
+                    <button type="submit" name="button_create" class="btn btn-success btn-sm float-right mr-2">
+                        <i class="fa fa-save"></i> Simpan
+                    </button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</section>
+<?php require_once ("partials/script.php") ?>
