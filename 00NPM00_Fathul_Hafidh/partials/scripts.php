20d001e3bd2ab0d0c3f2f29dafa730b6c0daa852
Fathul Hafidh <havid.aide@gmail.com>
2022-01-31 09:02:46 +0800

    update partial

--- 00NPM00_Fathul_Hafidh/partials/scripts.php
+++ 00NPM00_Fathul_Hafidh/partials/scripts.php
@@ -3,4 +3,4 @@
 <!-- Bootstrap 4 -->
 <script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="../assets//js/adminlte.min.js"></script>
+<script src="../assets/dist/js/adminlte.min.js"></script>
