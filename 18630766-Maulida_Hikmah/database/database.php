7957c5be5859427386f55ffb660855db2c6b7354
xtra-mushroom <maulidavip@gmail.com>
2022-01-31 10:09:37 +0800

    Menampilkan data lokasi

--- 18630766-Maulida_Hikmah/database/database.php
+++ 18630766-Maulida_Hikmah/database/database.php
@@ -8,7 +8,7 @@ class Database {
         $this->host = "localhost";
         $this->username = "lava";
         $this->password = "linolee";
-        $this->db_name = "master";
+        $this->db_name = "praktikum_presensi_penggajian";
     }
 
     public function getConnection()
