d741b1815e4f4a8df6cbe2d4839c5dbd3a1c0221
Noor Aftiah Pasayangan <nraftiah09@gmail.com>
2022-01-31 11:09:55 +0800

    Merapikan footer,nav dan sidebar

--- 18630908_Noor_Aftiah_Pasayangan/partials/footer.php
+++ 18630908_Noor_Aftiah_Pasayangan/partials/footer.php
@@ -1,8 +1,7 @@
-<footer class="main-footer">
-    <!-- To the right -->
+<footer class="main-footer">\
     <div class="float-right d-none d-sm-inline">
-        Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        <div class="float-right d-none d-sm-block">
+            <b>Version</b> 0.1.0
+        </div>
+        <strong>Copyright &copy; 2022 <a href="#">Praktikum Web</a>.</strong> All rights reserved.
 </footer>
