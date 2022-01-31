3605ddde2cec7634448b96e07fc361dce68eaed0
Fathul Hafidh <havid.aide@gmail.com>
2022-01-31 08:56:19 +0800

    remake

--- 00NPM00_Fathul_Hafidh/partials/footer.php
+++ 00NPM00_Fathul_Hafidh/partials/footer.php
@@ -1,6 +1,10 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b> 0.1.0
+ <!-- Main Footer -->
+ <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
     </div>
-    <strong>Copyright &copy; 2022 <a href="https://mirzayogy.github.io">Praktikum Web</a>.</strong> All rights reserved.
-</footer>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+<!-- ./wrapper -->
