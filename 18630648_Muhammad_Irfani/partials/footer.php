9e68ae8f9203a89949fe040d725636d9ad03c488
irfani12 <96363883+irfani12@users.noreply.github.com>
2022-01-31 09:45:34 +0800

    koneksi database dan datatables

--- 18630648_Muhammad_Irfani/partials/footer.php
+++ 18630648_Muhammad_Irfani/partials/footer.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 0.1.0
     </div>
-    <strong>Copyright &copy; 2022 <a href="https://mirzayogy.github.io">Praktikum Web</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2022 <a href="#">Praktikum Web</a>.</strong> All rights reserved.
 </footer>
