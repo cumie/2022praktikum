be13533905fe1d3abb2d7a2f6a7caec835642bcd
Noor Aftiah Pasayangan <nraftiah09@gmail.com>
2022-02-02 16:18:37 +0800

    Membuat Perintah Create Data

--- 18630908_Noor_Aftiah/pages/admin/lokasicreate.php
+++ 18630908_Noor_Aftiah/pages/admin/lokasicreate.php
@@ -1,3 +1,33 @@
+<?php
+if (isset($_POST['button_create'])) {
+    $database = new Database();
+    $db = $database->getConnection();
+    $validateSQL = "SELECT * FROM lokasi where nama_lokasi=?";
+    $stmt = $db->prepare($validateSQL);
+    $stmt->bindParam(1, $_POST['nama_lokasi']);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+?>
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            <h5><i class="icon fas-ban"></i> Gagal</h5>
+            Nama Lokasi Sudah Ada
+        </div>
+<?php
+    } else {
+        $insertSQL = "INSERT INTO lokasi SET nama_lokasi = ?";
+        $stmt = $db->prepare($insertSQL);
+        $stmt->bindParam(1, $_POST['nama_lokasi']);
+        if ($stmt->execute()) {
+            $_SESSION['hasil'] = true;
+            $_SESSION['pesan'] = "Berhasil Simpan Data";
+        } else {
+            $_SESSION['hasil'] = true;
+            $_SESSION['pesan'] = "Gagal Simpan Data";
+        }
+    }
+}
+?>
 <div class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
