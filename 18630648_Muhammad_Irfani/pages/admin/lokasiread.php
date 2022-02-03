3fdd8f471d2fd3e4e3db6b62e2556a28ce720460
irfani12 <96363883+irfani12@users.noreply.github.com>
2022-02-04 07:41:11 +0800

    update lokasiread

--- 18630648_Muhammad_Irfani/pages/admin/lokasiread.php
+++ 18630648_Muhammad_Irfani/pages/admin/lokasiread.php
@@ -1,29 +1,52 @@
 <?php include_once "partials/cssdatatables.php" ?>
-<!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
+        <?php
+        if (isset($_SESSION["hasil"])) {
+            if ($_SESSION["hasil"]) {
+        ?>
+                <div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    <h5><i class="icon fas-check"></i> Berhasil</h5>
+                    <?php echo $_SESSION["pesan"] ?>
+                </div>
+            <?php
+            } else {
+            ?>
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    <h5><i class="icon fas-check"></i> Gagal</h5>
+                    <?php echo $_SESSION["pesan"] ?>
+                </div>
+        <?php
+            }
+            unset($_SESSION['hasil']);
+            unset($_SESSION['pesan']);
+        }
+        ?>
         <div class="row mb-2">
             <div class="col-sm-6">
                 <h1 class="m-0">Lokasi</h1>
-            </div><!-- /.col -->
+            </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="?page=Home">Home</a></li>
-                    <li class="breadcrumb-item active">Lokasi</li>
+                    <li class="breadcrumb-item">
+                        <a href="?page=home">Home</a>
+                    </li>
+                    <li class="breadcrumb-item">Lokasi</li>
                 </ol>
             </div>
         </div>
     </div>
 </div>
-<!-- /.content-header -->
 
-<!-- Main content -->
 <div class="content">
     <div class="card">
         <div class="card-header">
             <h3 class="card-title">Data Lokasi</h3>
             <a href="?page=lokasicreate" class="btn btn-success btn-sm float-right">
-                <i class="fa fa-plus-circle"></i> Tambah Data</a>
+                <i class="fa fa-plus-circle"></i> Tambah Data
+            </a>
         </div>
         <div class="card-body">
             <table id="mytable" class="table table-bordered table-hover">
@@ -42,13 +65,15 @@
                     </tr>
                 </tfoot>
                 <tbody>
-
                     <?php
                     $database = new Database();
                     $db = $database->getConnection();
+
                     $selectSql = "SELECT * FROM lokasi";
+
                     $stmt = $db->prepare($selectSql);
                     $stmt->execute();
+
                     $no = 1;
                     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                     ?>
@@ -59,7 +84,7 @@
                                 <a href="?page=lokasiupdate&id=<?php echo $row['id'] ?>" class="btn btn-primary btn-sm mr-1">
                                     <i class="fa fa-edit"></i> Ubah
                                 </a>
-                                <a href="?page=lokasidelete&id=<?php echo $row['id'] ?>" class="btn btn-danger btn-sm" onClick="javascript: return confirm('Konfirmasi data akan dihapus?');">
+                                <a href="?page=lokasidelete&id=<?php echo $row['id'] ?>" class="btn btn-danger btn-sm" onclick="javascript: return confirm('Konfirmasi data akan dihapus?');">
                                     <i class="fa fa-trash"></i> Hapus
                                 </a>
                             </td>
@@ -68,17 +93,12 @@
                     }
                     ?>
                 </tbody>
-
-
             </table>
-
         </div>
     </div>
 </div>
-<!-- /.content -->
 
-<?php include "partials/scripts.php" ?>
-
+<?php include_once "partials/scripts.php" ?>
 <?php include_once "partials/scriptsdatatables.php" ?>
 <script>
     $(function() {
