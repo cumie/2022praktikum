ff5f9a4fdcad61c9a01bfab45313221627243366
Noor Aftiah Pasayangan <nraftiah09@gmail.com>
2022-02-07 09:28:00 +0800

    Membuat halaman tampil data karyawan

--- 18630908_Noor_Aftiah/partials/sidebar.php
+++ 18630908_Noor_Aftiah/partials/sidebar.php
@@ -58,7 +58,7 @@
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="#" class="nav-link">
+                             <a href="?page=karyawanread" class="nav-link">
                                  <i class="nav-icon far fa-circle"></i>
                                  <p>Karyawan</p>
                              </a>
